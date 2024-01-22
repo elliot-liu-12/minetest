e4ba7955c69744f27df58626297ac44acac6b570
aririsu <allensun0419@yahoo.com>
2024-01-21 23:24:36 -0500

    Nodemeta class separation and compilation completed

---
+++ src/script/native_api/native_nodemeta.cpp
@@ -0,0 +1,28 @@
+#include "lua_api/l_nodemeta.h"
+#include "lua_api/l_internal.h"
+#include "lua_api/l_inventory.h"
+#include "common/c_content.h"
+#include "serverenvironment.h"
+#include "map.h"
+#include "mapblock.h"
+#include "server.h"
+#include "native_nodemeta.h"
+
+void nativeModApiNodemeta::n_get_inventory(NodeMetaRef* ref)
+{
+	ref->NodeMetaRef::getmeta(true);
+}
+
+NodeMetadata* nativeModApiNodemeta::n_mark_as_private(NodeMetadata *meta, NodeMetaRef* ref, int flag)
+{
+	if (flag == 0) {
+		meta = dynamic_cast<NodeMetadata *>(ref->getmeta(true));
+		assert(meta);
+		return meta;
+	}
+	
+	if (flag == 1) {
+		ref->reportMetadataChange();
+		return meta;
+	}
+}
